9ae9899b990210a6afe33e79afa27b128d07631e
xiaoming <2014500726@smail.xtu.edu.cn>
2024-10-10 23:43:40 +0800

    Refactor sortBoxes function to handle different sorting orders
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- qfacefusion_api.cpp
+++ qfacefusion_api.cpp
@@ -21,8 +21,62 @@ FaceFusion::~FaceFusion() {
 	delete m_enhance_face_net;
 }
 
+void FaceFusion::sortBoxes(vector<Bbox> &boxes, uint32_t order) {
+	//根据order来排序
+	// order = 0: 左右排序
+	// order = 1: 右左排序
+	// order = 2: 上下排序
+	// order = 3: 下上排序
+	if(order == 0) {
+		sort(boxes.begin(), boxes.end(), [](const Bbox &a, const Bbox &b) {
+			return a.xmin < b.xmin;
+		});
+	} else if(order == 1) {
+		sort(boxes.begin(), boxes.end(), [](const Bbox &a, const Bbox &b) {
+			return a.xmin > b.xmin;
+		});
+	} else if(order == 2) {
+		sort(boxes.begin(), boxes.end(), [](const Bbox &a, const Bbox &b) {
+			return a.ymin < b.ymin;
+		});
+	} else if(order == 3) {
+		sort(boxes.begin(), boxes.end(), [](const Bbox &a, const Bbox &b) {
+			return a.ymin > b.ymin;
+		});
+	}
+}
+
+void FaceFusion::sortBoxesWithKp5(vector<Bbox> &boxes, vector<vector<FaceFusionUtils::KeyPoint>> &kp5_raw, uint32_t order) {
+	//根据order来排序, 同时把kp5_raw也根据boxes的顺序对应调换
+	// order = 0: 左右排序
+	// order = 1: 右左排序
+	// order = 2: 上下排序
+	// order = 3: 下上排序
+	vector<Bbox> old_boxes = boxes;
+	if(order == 0) {
+		sort(boxes.begin(), boxes.end(), [](const Bbox &a, const Bbox &b) {
+			return a.xmin < b.xmin;
+		});
+	} else if(order == 1) {
+		sort(boxes.begin(), boxes.end(), [](const Bbox &a, const Bbox &b) {
+			return a.xmin > b.xmin;
+		});
+	} else if(order == 2) {
+		sort(boxes.begin(), boxes.end(), [](const Bbox &a, const Bbox &b) {
+			return a.ymin < b.ymin;
+		});
+	} else if(order == 3) {
+		sort(boxes.begin(), boxes.end(), [](const Bbox &a, const Bbox &b) {
+			return a.ymin > b.ymin;
+		});
+	}
+	//TODO: kp5_raw的顺序调换
+    //vector<vector<FaceFusionUtils::KeyPoint>> new_kp5_raw;
+	//kp5_raw = new_kp5_raw;
+}
+
 int FaceFusion::runSwap(const cv::Mat &source_img, const cv::Mat &target_img, cv::Mat &output_img,
-			uint32_t id, bool multipleFace, std::function<void(uint64_t)> progress) {
+			uint32_t id, uint32_t order, bool multipleFace, std::function<void(uint64_t)> progress) {
 	if(source_img.empty() || target_img.empty()){
         return -1;
     }
@@ -48,6 +102,7 @@ int FaceFusion::runSwap(const cv::Mat &source_img, const cv::Mat &target_img, cv
 	if(boxes.empty()) {
 		return -1;
 	}
+	sortBoxes(boxes, order);
 	if(multipleFace) {
     	if(progress) progress(50);
 		output_img = target_img;
@@ -101,7 +156,7 @@ int FaceFusion::setSource(const cv::Mat &source_img) {
 }
 
 int FaceFusion::runSwap(const cv::Mat &target_img, cv::Mat &output_img,
-			uint32_t id, bool multipleFace, std::function<void(uint64_t)> progress) {
+			uint32_t id, uint32_t order, bool multipleFace, std::function<void(uint64_t)> progress) {
 	if(!m_source) {
 		return -1;
 	}
@@ -118,6 +173,7 @@ int FaceFusion::runSwap(const cv::Mat &target_img, cv::Mat &output_img,
 	if(boxes.empty()) {
 		return -1;
 	}
+	sortBoxes(boxes, order);
 	if(multipleFace) {
     	if(progress) progress(30);
 		output_img = target_img;
@@ -150,7 +206,7 @@ int FaceFusion::runSwap(const cv::Mat &target_img, cv::Mat &output_img,
 	return 0;
 }
 
-int FaceFusion::setDetect(const cv::Mat &source_img, cv::Mat &output_img) {
+int FaceFusion::setDetect(const cv::Mat &source_img, cv::Mat &output_img, uint32_t order) {
 	if(source_img.empty()){
         return -1;
     }
@@ -160,8 +216,9 @@ int FaceFusion::setDetect(const cv::Mat &source_img, cv::Mat &output_img) {
 	vector<Bbox> boxes;
     std::vector<std::vector<FaceFusionUtils::KeyPoint>> kp5_raw;
     m_detect_face_net->detect_with_kp5(source_img, boxes, kp5_raw);
+	sortBoxesWithKp5(boxes, kp5_raw, order);
 	cv::Mat temp_vision_frame = source_img.clone();
-	for (int i = 0; i < boxes.size(); i++){
+    for (size_t i = 0; i < boxes.size(); i++){
 		cv::rectangle(temp_vision_frame, cv::Point(boxes[i].xmin, boxes[i].ymin), cv::Point(boxes[i].xmax, boxes[i].ymax), cv::Scalar(0, 255, 0), 2);
 		for (int j = 0; j < 5; j++){
 			cv::circle(temp_vision_frame, cv::Point(kp5_raw[j][i].x, kp5_raw[j][i].y), 2, cv::Scalar(0, 255, 0), 2);
@@ -174,7 +231,10 @@ int FaceFusion::setDetect(const cv::Mat &source_img, cv::Mat &output_img) {
     return 0;
 }
 
-int FaceFusion::faceSwap(const string &source_path, const string &target_path, const string &output_path, uint32_t id, bool multipleFace) {
+int FaceFusion::faceSwap(const string &source_path, 
+						 const string &target_path, 
+						 const string &output_path, 
+						 uint32_t id, uint32_t order, bool multipleFace) {
 	Mat source_img = imread(source_path);
 	Mat target_img = imread(target_path);
 	if(source_img.empty() || target_img.empty()){
@@ -206,6 +266,7 @@ int FaceFusion::faceSwap(const string &source_path, const string &target_path, c
 	if(boxes.empty()) {
 		return -1;
 	}
+	sortBoxes(boxes, order);
 	Mat resultimg = target_img;
 	if(multipleFace) {
         for (size_t i = 0; i < boxes.size(); i++) {
@@ -230,7 +291,10 @@ int FaceFusion::faceSwap(const string &source_path, const string &target_path, c
     return 0;
 }
 
-int FaceFusion::faceSwap(const Mat &source_img, const Mat &target_img, Mat &output_img, uint32_t id, bool multipleFace) {
+int FaceFusion::faceSwap(const Mat &source_img, 
+						 const Mat &target_img, 
+						 Mat &output_img, 
+						 uint32_t id, uint32_t order, bool multipleFace) {
 	if(source_img.empty() || target_img.empty()){
         return -1;
     }
@@ -259,6 +323,7 @@ int FaceFusion::faceSwap(const Mat &source_img, const Mat &target_img, Mat &outp
 	if(boxes.empty()) {
 		return -1;
 	}
+	sortBoxes(boxes, order);
 	if(multipleFace) {
 		output_img = target_img;
         for (size_t i = 0; i < boxes.size(); i++) {
